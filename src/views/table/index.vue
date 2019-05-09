c762a98b8ba4a955e1bf071f88deb83a676656a3
qunqunzq <qun10086>
2019-05-09 17:44:57 +0800

    1、主图上传

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -38,6 +38,27 @@
       </el-table-column>
       <el-table-column
         align="center"
+        prop="img_src"
+        label="主图"
+      >
+        <template slot-scope="scope">
+          <el-upload
+            :data="{'id':scope.row.id}"
+            class="avatar-uploader"
+            action="/api/kantu/uploadZhuImage"
+            :headers = "headers"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload">
+            <img v-if="'http://localhost:6060/232.jpg'" :src="'http://localhost:6060/232.jpg'" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </template>
+      </el-table-column>
+
+
+      <el-table-column
+        align="center"
         prop="create_time"
         label="创建时间"
          >
@@ -120,11 +141,15 @@
 
 <script>
 import Pagination from '@/components/Pagination'
-
+import { getToken } from '@/utils/auth'
 export default {
   components: { Pagination },
   data() {
     return {
+      imageUrl: 'http://localhost:6060/232.jpg',
+      headers:{
+        "Authorization": 'Bearer '+getToken()
+      },
       dialogVisible:false,
       total:0,
       listQuery:{
@@ -242,6 +267,47 @@ export default {
         }
       });
     },
+
+    handleAvatarSuccess(res, file) {
+
+
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg';
+      const isLt2M = file.size / 1024 / 1024 < 10;
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!');
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 10MB!');
+      }
+      return isJPG && isLt2M;
+    },
   }
 }
 </script>
+<style>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
